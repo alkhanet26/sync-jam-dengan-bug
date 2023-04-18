afe98a6725f8e36b0f2af2c89bed22a897889c2f
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-04-18 09:15:54 +0700

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # ping by AlkhaNET
-#
+
+bug="$1"
+
 echo -e "ping.sh : Starting ping koneksi"
 logger "ping.sh : Starting ping koneksi"
-	ping -c 5 8.8.8.8 | grep round-trip > /tmp/cping
+	ping -c 5 ${bug} | grep round-trip > /tmp/cping
 	
 	pms=$(cat /tmp/cping | cut -d/ -f4)
 	msa=$(cat /tmp/cping | grep -o ms)
@@ -12,7 +14,7 @@ if [ $msa = "ms" ]; then
 	echo -e "ping.sh : time = $pms"
 	logger "ping.sh : time = $pms"
 else
-	/sbin/jam.sh
+	/sbin/jam.sh ${bug}
 fi	
 
 rm /tmp/cping
