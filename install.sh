d277b8976db2fb28bc44d2d90a9b6a7e2981a9bc
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-06-18 21:22:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 # ping by AlkhaNET
 
 {
+echo -e "Install Paket Pendukung"
+sleep 1
+opkg update && opkg install curl libcurl
+clear
 echo -e "Downloading Binaries"
+sleep 2
 wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/jam.sh" -O /sbin/jam.sh && chmod +x /sbin/jam.sh
 wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/ping.sh" -O /sbin/ping.sh && chmod +x /sbin/ping.sh
 wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/cleanup.sh" -O /sbin/cleanup.sh && chmod +x /sbin/cleanup.sh
