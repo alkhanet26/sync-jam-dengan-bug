ff7e0fc6b6790f4de80c480a5fdfd1e18e6ea45b
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-06-18 21:24:14 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ opkg update && opkg install curl libcurl
 clear
 echo -e "Downloading Binaries"
 sleep 2
-wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/jam.sh" -O /sbin/jam.sh && chmod +x /sbin/jam.sh
+wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/jam" -O /sbin/jam && chmod +x /sbin/jam
 wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/ping.sh" -O /sbin/ping.sh && chmod +x /sbin/ping.sh
-wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/cleanup.sh" -O /sbin/cleanup.sh && chmod +x /sbin/cleanup.sh
+wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/cleanup" -O /sbin/cleanup && chmod +x /sbin/cleanup
 echo -e "Updating Binaries Selesai"
 sleep 3
 clear && bash --login
