d12cdbcb38f49a9120d2c235f09a2182f8a887a9
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-07-06 11:00:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ opkg update && opkg install curl libcurl
 clear
 echo -e "Downloading Binaries"
 sleep 2
-wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/jam" -O /sbin/jam && chmod +x /sbin/jam
-wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/ping.sh" -O /sbin/ping.sh && chmod +x /sbin/ping.sh
-wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/cleanup" -O /sbin/cleanup && chmod +x /sbin/cleanup
-wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/release_ram.lua" -O /usr/lib/lua/luci/controller/release_ram.lua
+wget -q "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/jam" -O /sbin/jam && chmod +x /sbin/jam
+wget -q "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/ping.sh" -O /sbin/ping.sh && chmod +x /sbin/ping.sh
+wget -q "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/cleanup" -O /sbin/cleanup && chmod +x /sbin/cleanup
+wget -q "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/release_ram.lua" -O /usr/lib/lua/luci/controller/release_ram.lua
 echo -e "Updating Binaries Selesai"
 sleep 3
 clear && bash --login
