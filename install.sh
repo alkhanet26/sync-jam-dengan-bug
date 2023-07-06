3f9a085d2b6051dfd4af7ea1e679a11051d7437c
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-07-06 11:03:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+o#!/bin/bash
 # ping by AlkhaNET
 
 {
@@ -8,10 +8,14 @@ opkg update && opkg install curl libcurl
 clear
 echo -e "Downloading Binaries"
 sleep 2
-wget -q "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/jam" -O /sbin/jam && chmod +x /sbin/jam
-wget -q "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/ping.sh" -O /sbin/ping.sh && chmod +x /sbin/ping.sh
-wget -q "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/cleanup" -O /sbin/cleanup && chmod +x /sbin/cleanup
-wget -q "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/release_ram.lua" -O /usr/lib/lua/luci/controller/release_ram.lua
+echo -e "=   25%" && wget -q "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/jam" -O /sbin/jam && chmod +x /sbin/jam
+sleep 1
+echo -e "==  50%" && wget -q "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/ping.sh" -O /sbin/ping.sh && chmod +x /sbin/ping.sh
+sleep 1
+echo -e "=== 75%" && wget -q "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/cleanup" -O /sbin/cleanup && chmod +x /sbin/cleanup
+sleep 1
+echo -e "====100%" && wget -q "https://raw.githubusercontent.com/alkhanet26/sync-jam-dengan-bug/main/release_ram.lua" -O /usr/lib/lua/luci/controller/release_ram.lua
+sleep 2
 echo -e "Updating Binaries Selesai"
 sleep 3
 clear && bash --login
